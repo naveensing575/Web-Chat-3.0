4708a86b00560b3e6220c9f2b7f06c5df52579bf
naveen.singh <naveen.singh@itelasoft.com>
2024-11-25 14:35:26 +0530

    update tailwind config with various daisy UI themes

--- frontend/src/components/AuthImagePattern.tsx
+++ frontend/src/components/AuthImagePattern.tsx
@@ -22,7 +22,7 @@ const AuthImagePattern: React.FC<AuthImagePatternProps> = ({
             />
           ))}
         </div>
-        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+        <h2 className="text-2xl font-bold mb-4 theme-text">{title}</h2>
         <p className="text-base-content/60">{subtitle}</p>
       </div>
     </div>
